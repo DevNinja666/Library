7b9591441d28ddcaa9f96616e1c6040d44ab46ca
Elnur <elnurgasanovv07@gmail.com>
2026-03-26 00:37:54 +0400

    Create booksData.js

---
+++ src/booksData.js
@@ -0,0 +1,49 @@
+export const allBooks = [
+  {
+    id: 1,
+    title: "Дюна",
+    author: "Фрэнк Герберт",
+    genre: "Фантастика",
+    rating: 4.8,
+    cover: "https://covers.openlibrary.org/b/id/8168691-L.jpg",
+    description: "Борьба за контроль над планетой Арракис."
+  },
+  {
+    id: 2,
+    title: "1984",
+    author: "Джордж Оруэлл",
+    genre: "Фантастика",
+    rating: 4.7,
+    cover: "https://covers.openlibrary.org/b/id/7222246-L.jpg",
+    description: "Мир тотального контроля."
+  },
+  {
+    id: 3,
+    title: "Шерлок Холмс",
+    author: "Конан Дойл",
+    genre: "Детектив",
+    rating: 4.9,
+    cover: "https://covers.openlibrary.org/b/id/8231856-L.jpg",
+    description: "Лучшие расследования."
+  },
+  {
+    id: 4,
+    title: "Преступление и наказание",
+    author: "Достоевский",
+    genre: "Роман",
+    rating: 4.6,
+    cover: "https://covers.openlibrary.org/b/id/8231996-L.jpg",
+    description: "Моральный выбор."
+  },
+  {
+    id: 5,
+    title: "Гарри Поттер",
+    author: "Роулинг",
+    genre: "Детям",
+    rating: 4.9,
+    cover: "https://covers.openlibrary.org/b/id/7984916-L.jpg",
+    description: "Мир магии."
+  }
+];
+
+export const genres = ["Все", "Фантастика", "Детектив", "Роман", "Детям"];
