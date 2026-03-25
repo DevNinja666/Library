ca33397c6d4d38607768bf7dec83a92de8111e1b
Elnur <elnurgasanovv07@gmail.com>
2026-03-26 00:21:37 +0400

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+      <p>© 2026 Online Library</p>
+      <p>📞 +994 50 123 45 67</p>
+    </footer>
+  );
+}
+
+export default Footer;
